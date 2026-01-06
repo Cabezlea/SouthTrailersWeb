3241f9f5b9bff44193992c7539376dfff0addb02
Cabezlea <Leocabezas@icloud.com>
2026-01-06 11:01:47 -0600

    routing set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-export default function App() {
-  return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-950">
-        <h1 className="text-4xl font-bold text-white">Tailwind is ON</h1>
-      </div>
-  );
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import Shipping from "./pages/Shipping";
+import Contact from "./pages/Contact";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/shipping" element={<Shipping />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
+export default App;
